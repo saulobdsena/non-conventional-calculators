c7a9d2b8b483071ccaa92ffe291986edf9223d0b
Saulo Sena <saulobdsena@gmail.com>
2026-07-23 19:17:21 -0300

    feat: add calculator_1 for division, squaring, multiplication and printing input data (Calculator1) feat: update calculators routes to include Calculator1 (src/main/routes/calculators.py) feat: create Calculator1 class with required methods (src/calculators/calculator_1.py) feat: add README.md for project documentation

---
+++ src/calculators/calculator_1.py
@@ -0,0 +1,21 @@
+from flask import request as FlaskRequest
+
+class Calculator1:
+
+    """
+    The first part is divided by 4, and the result is added to 7.
+    After that, the result is squared and multiplied by a value of 0.257
+    """
+    
+
+    def calculate(self, request: FlaskRequest) -> Dict:
+        body = request.json
+        input_data = self.__validate_body(body)
+        print(input_data)
+    
+    def __validate_body(self, body: Dict) -> float:
+        if "number" not in body:
+            raise Exception("Body mal formatado")
+
+        input_data = body["number"]
+        return input_data
--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, jsonify, request
-
+from src.calculators.calculator_1 import Calculator1
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    print(request)
+    calc = Calculator1()
+    calc.calculate(request)
     return jsonify({"sucess": True}), 200

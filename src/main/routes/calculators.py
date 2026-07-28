c6916ae556d49b1721516a91a8bb7720a99e6e80
Saulo Sena <saulobdsena@gmail.com>
2026-07-28 16:27:24 -0300

    "Add pytest for calculator 1 test and refactor calculator function to return response"

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -6,5 +6,5 @@ calc_route_bp = Blueprint("calc_routes", __name__)
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
     calc = Calculator1()
-    calc.calculate(request)
-    return jsonify({"sucess": True}), 200
+    response = calc.calculate(request)
+    return jsonify(response), 200
